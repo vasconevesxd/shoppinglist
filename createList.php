97c8bbf3b1c29bd3601b17373c5b839f04254b78
dannguitar <123259428+dannguitar@users.noreply.github.com>
2023-02-05 20:17:10 +0000

    corrigir href do index

--- createList.php
+++ createList.php
@@ -152,7 +152,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="index.php">Shopping List</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/shoppinglist">Shopping List</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
